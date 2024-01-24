b761e548274ff2a2819a1b8b477b948efdfdd097
Auban <aubanlabie@gmail.com>
2024-01-24 17:29:57 +0100

    fix sass & components

---
+++ src/components/Calculatrice.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/scss/calculatrice.scss'
+
+function Calculatrice() {
+    return (
+        <section>
+            <h1>TEST REACT</h1>
+        </section>
+    );
+}
+
+export default Calculatrice;
